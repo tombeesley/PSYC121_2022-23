d20e35142373780416671ed842ae8a2b34887de2
tombeesley <t.beesley@lancaster.ac.uk>
2021-11-19 14:53:12 +0000

    final before release

--- files/Week_7/Week_7_script.R
+++ files/Week_7/Week_7_script.R
@@ -9,7 +9,7 @@ data_w7 <- read_csv("uk_salary.csv")
 # quick look at the salary estimates
 data_w7 %>% 
   ggplot() + 
-  geom_histogram(aes()) # EDIT THIS CODE FOR Q2
+  geom_histogram(aes(x = uk_salary)) # EDIT THIS CODE FOR Q2
 
 # let's look at those unusual values
 data_w7 %>% 
@@ -34,14 +34,21 @@ data_w7_f%>%
 
 # What is the mean estimate? 
 mean() # EDIT THIS CODE FOR Q7
+sd() # you might also calculate the standard deviation
 
 # Let's check if the mean estimate is significantly different from the true salary
 t.test(x = , mu = ) # You will need to edit this for Q8
 
+# EXTRA
+# this is a "by hand" t statistic - you can add the column of salary estimates here and check with the result of t.test
+# check with staff if you're not sure
+t_val <- (mean()-30000)/(sd()/sqrt(nrow())) 
+
 # TASK 3
 
-# plot the data for the 
-data_w7 %>% 
+# plot the uk_salary data using geom_density
+# EDIT THIS CODE FOR Q11-14
+data_w7_f %>% 
   ggplot() + 
-  geom_density(aes()) # EDIT THIS CODE FOR Q
+  geom_density(aes())
 
