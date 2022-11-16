5a6c7ccbf6c993a8f2cabfaf1ba3bfc5fb26f7fa
tombeesley <t.beesley@lancaster.ac.uk>
2022-11-16 14:56:33 +0000

    draft of Week 7

---
+++ files/Week_7/Week_7_script.R
@@ -0,0 +1,47 @@
+library(tidyverse) 
+options(scipen = 999) # this stops numbers appearing as scientific notation
+
+# TASK 1
+
+# read in the data with read_csv
+
+
+
+# quick look at the salary estimates
+*MISSING_data_set_name* %>% 
+  ggplot(aes(*MISSING_DV*))) + 
+  geom_histogram() # EDIT THIS CODE FOR Q2
+
+# let's look at those unusual values
+*MISSING_data_set_name* %>% 
+  arrange(desc(*MISSING_DV*)) # arrange the data according to descending values of uk_salary estimates
+
+# let's remove those really high values
+*MISSING_data_set_name* %>% # with the data...
+  filter(*MISSING*) # EDIT THIS CODE FOR Q4 
+
+
+
+
+
+# TASK 2
+
+# What is the mean estimate? 
+mean(data_w7_f$uk_salary) # EDIT THIS CODE FOR Q7
+sd() # you might also calculate the standard deviation
+
+# Let's check if the mean estimate is significantly different from the true salary
+t.test(x = data_w7_f$uk_salary, mu = 30000) # You will need to edit this for Q8
+
+# EXTRA
+# this is a "by hand" t statistic - you can add the column of salary estimates here and check with the result of t.test
+# check with staff if you're not sure
+t_val <- (mean(data_w7_f$uk_salary) - 30000) / (sd(data_w7_f$uk_salary)/sqrt(length(data_w7_f$uk_salary))) # denominator
+
+# TASK 3
+
+# more filtering practice
+
+data_set_name %>% # change this to your data object
+  filter(home_location_in_UK == "NW" & sibling_order == "oldest")
+
