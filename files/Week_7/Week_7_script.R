2a0aa4cf687eb93d1a2c682cd9371f3e6d529b89
tombeesley <t.beesley@lancaster.ac.uk>
2022-11-18 09:28:51 +0000

    update instructions and zip

--- files/Week_7/Week_7_script.R
+++ files/Week_7/Week_7_script.R
@@ -3,9 +3,7 @@ options(scipen = 999) # this stops numbers appearing as scientific notation
 
 # TASK 1
 
-# read in the data with read_csv
-
-
+# read in the data with read_csv - Q1
 
 # quick look at the salary estimates
 *MISSING_data_set_name* %>% 
@@ -27,21 +25,22 @@ options(scipen = 999) # this stops numbers appearing as scientific notation
 # TASK 2
 
 # What is the mean estimate? 
-mean(data_w7_f$uk_salary) # EDIT THIS CODE FOR Q7
+mean(*MISSING_data_set_name*$uk_salary) # EDIT THIS CODE FOR Q8
 sd() # you might also calculate the standard deviation
 
 # Let's check if the mean estimate is significantly different from the true salary
-t.test(x = data_w7_f$uk_salary, mu = 30000) # You will need to edit this for Q8
+t.test(x = *MISSING_data_set_name*$uk_salary, mu = 30000) # You will need to edit this for Q9
 
 # EXTRA
 # this is a "by hand" t statistic - you can add the column of salary estimates here and check with the result of t.test
 # check with staff if you're not sure
-t_val <- (mean(data_w7_f$uk_salary) - 30000) / (sd(data_w7_f$uk_salary)/sqrt(length(data_w7_f$uk_salary))) # denominator
+t_val <- (mean(*MISSING_data_set_name*$uk_salary) - 30000) / (sd(*MISSING_data_set_name*$uk_salary)/sqrt(nrow(*MISSING_data_set_name*$uk_salary))) # denominator
 
 # TASK 3
 
 # more filtering practice
 
-data_set_name %>% # change this to your data object
+*MISSING_data_set_name* %>% # change this to your data object
   filter(home_location_in_UK == "NW" & sibling_order == "oldest")
 
+# add more filter commands for Q12-17
